a8226bb25b17f9e9021d176def9c0a055ac1ebad
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2023-12-15 11:30:25 +0530

    Update 9.palindrome-number.java

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -1,9 +1,3 @@
-/*
- * @lc app=leetcode id=9 lang=java
- *
- * [9] Palindrome Number
- */
-
 class Solution {
     public boolean isPalindrome(int x) {
         //Constraints
@@ -36,5 +30,4 @@ class Solution {
         }
     }
 }
-// @lc code=end
-
+//https://github.com/EmilWijayasekara
